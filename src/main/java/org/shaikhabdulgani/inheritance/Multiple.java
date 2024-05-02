08ba517ffa7aeb94d962449166e007584c479663
abdulgani-shaikh <abdulgani.shaikh@neosoftmail.com>
2024-05-02 18:47:39 +0530

    implement multiple inheritance

--- src/main/java/org/shaikhabdulgani/inheritance/Multiple.java
+++ src/main/java/org/shaikhabdulgani/inheritance/Multiple.java
@@ -1,7 +1,38 @@
 package org.shaikhabdulgani.inheritance;
 
+//Java doesn't support multiple inheritance directly.
+//But you can use interface to implement multiple inheritance
 public class Multiple {
     public static void main(String[] args) {
-        System.out.println("Java doesn't support multiple inheritance");
+        Human john = new Human("John");
+        john.pedal();
+        john.swim();
+    }
+}
+
+interface Cyclist{
+    void pedal();
+}
+
+interface Swimmer{
+    void swim();
+}
+
+class Human implements Cyclist, Swimmer {
+
+    private String name;
+
+    Human(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public void pedal() {
+        System.out.printf("%s is Peddling\n",name);
+    }
+
+    @Override
+    public void swim() {
+        System.out.printf("%s is Swimming\n",name);
     }
 }
