db006501532f9bee9a3928647b9f33ab1d4f3538
abdulgani-shaikh <abdulgani.shaikh@neosoftmail.com>
2024-05-02 17:12:18 +0530

    initial commit

---
+++ src/main/java/org/shaikhabdulgani/inheritance/SingleInheritance.java
@@ -0,0 +1,52 @@
+package org.shaikhabdulgani.inheritance;
+
+class Vehicle {
+    private String brand;
+    private String model;
+
+    public Vehicle(String brand, String model) {
+        this.brand = brand;
+        this.model = model;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    void start(){
+        System.out.println("Starting...");
+        System.out.println("Started");
+    }
+}
+
+class Car extends Vehicle{
+
+    private int numberOfTires;
+
+    public Car(String brand, String model, int numberOfTires) {
+        super(brand, model);
+        this.numberOfTires = numberOfTires;
+    }
+
+    public int getNumberOfTires() {
+        return numberOfTires;
+    }
+
+    @Override
+    void start() {
+        System.out.println("Car is starting.");
+    }
+}
+
+public class SingleInheritance {
+    public static void main(String[] args) {
+        Car car = new Car("Audi","A4",4);
+        car.start();
+        System.out.println("Car has "+car.getNumberOfTires()+" tires");
+        System.out.printf("Car brand is %s and model is %s\n",car.getBrand(),car.getModel());
+    }
+}
